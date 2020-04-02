6791d9698396b1da6ed0b6a8620ead0d406ad24e
songyachun <songyachun@yeah.net>
2020-04-02 14:29:50 +0800

    添加404页面使用Flask-Bootstrap

--- Flask Web Development/Chapter 3/hello.py
+++ Flask Web Development/Chapter 3/hello.py
@@ -1,5 +1,8 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -10,6 +13,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
