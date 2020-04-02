8157ba6827f27ef3b1e1888f88241c55256f801e
songyachun <songyachun@yeah.net>
2020-04-02 09:50:31 +0800

    Charper 3

--- Flask Web Development/Chapter 2/hello.py
+++ Flask Web Development/Chapter 2/hello.py
@@ -1,10 +1,21 @@
+from flask import make_response
+from flask import request
 from flask import Flask
 app = Flask(__name__)
 
-
+from flask import redirect
+from flask import abort
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    # return '<h1>Hello World!</h1>'
+    # user_agent = request.headers.get('User-Agent')
+    # return '<p> Your browser is {}</p>'.format(user_agent)
+    # return '<h1>Bad Request<h1/>',400
+    # response = make_response('<h1>This document carries a cookie</h1>')
+    # response.set_cookie('answer', '42')
+    # return response
+    return redirect('http://www.example.com')
+
 
 
 @app.route('/user/<name>')
