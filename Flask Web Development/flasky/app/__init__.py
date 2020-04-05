92602ddb598f252329a9b048e23f713fcf090851
songyachun <songyachun@yeah.net>
2020-04-05 16:29:38 +0800

    login and logiut commit

--- Flask Web Development/flasky/app/__init__.py
+++ Flask Web Development/flasky/app/__init__.py
@@ -7,14 +7,14 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
-login_manager=LoginManager()
-login_manager.login_view='auth.login'
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
+login_manager=LoginManager()
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -31,6 +31,6 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
     return app
--- Flask Web Development/flasky/app/auth/views.py
+++ Flask Web Development/flasky/app/auth/views.py
@@ -1,16 +1,19 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from flask_login import login_user,login_required,logout_user
-from .forms import LoginForm
+from flask_login import login_user, login_required, logout_user, current_user
+from .. import db
+from ..email import mail
+from .forms import LoginForm, RegistrationForm
 from ..models import User
 import pysnooper
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 @pysnooper.snoop()
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             next = request.args.get('next')
@@ -21,12 +24,22 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
-@auth.route('/secret')
-def secret():
-    return 'Only authenticated users are allowed!'
-
 @auth.route('/logout')
+@login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data.lower(),
+                    username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login. ')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
