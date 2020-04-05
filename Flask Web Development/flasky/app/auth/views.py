f07e78710bca70733fabebd28b9384f9c61372ac
songyachun <songyachun@yeah.net>
2020-04-05 15:10:17 +0800

    login

--- Flask Web Development/flasky/app/auth/views.py
+++ Flask Web Development/flasky/app/auth/views.py
@@ -1,11 +1,32 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from flask_login import login_required
+from flask_login import login_user,login_required,logout_user
+from .forms import LoginForm
+from ..models import User
+import pysnooper
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
+@pysnooper.snoop()
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
+
 
 @auth.route('/secret')
 def secret():
     return 'Only authenticated users are allowed!'
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
